64423ab632faa81eeeabd363a8747c66803a7d04
ZnoGouDj <znogoud3@gmail.com>
2022-03-29 12:41:52 +0200

    Add Cart Items/styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { useState } from 'react';
 import { useQuery } from 'react-query';
 // Components
 import Item from './Item/Item';
+import Cart from './Cart/Cart';
 import { Drawer, LinearProgress, Grid, Badge } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 // Styles
-import { Wrapper } from './App.styles';
+import { Wrapper, StyledButton } from './App.styles';
 // Types
 export type CartItemType = {
   id: number;
@@ -21,10 +22,12 @@ const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
   const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
   console.log({ data });
 
-  const getTotalItems = () => null;
+  const getTotalItems = (items: CartItemType[]) => items.reduce((ack: number, items) => ack + items.amount, 0);
 
   const handleAddToCart = (clickedItem: CartItemType) => null;
 
@@ -35,6 +38,14 @@ const App = () => {
 
   return (
     <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
+        <Cart cartItems={cartItems} addToCart={handleAddToCart} removeFromCart={handleRemoveFromCart} />
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color="error">
+          <AddShoppingCart />
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map(item => (
           <Grid item key={item.id} xs={12} sm={4}>
