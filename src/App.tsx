6266e938128b6ef0f7b175517eb66c4652bc80b9
ZnoGouDj <znogoud3@gmail.com>
2022-03-29 11:57:34 +0200

    Item

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,33 @@ import { Drawer, LinearProgress, Grid, Badge } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 // Styles
 import { Weapper } from './App.styles';
+// Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
+  console.log({ data });
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong...</div>;
+
   return <div className="App">Start</div>;
 };
 
